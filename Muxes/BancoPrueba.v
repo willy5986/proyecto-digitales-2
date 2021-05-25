7b08aa1586aac845188256e89f167c798f0e1f6e
willy5986 <willy59861@gmail.com>
2021-05-25 15:36:55 -0600

    agrega estructural a banco

--- Muxes/BancoPrueba.v
+++ Muxes/BancoPrueba.v
@@ -3,10 +3,11 @@
 `include "probador.v"
 `include "muxL2.v" // Ahora se incluyen los archivos de la descripcion 
 // conductual y del probador
+`include "muxL2_estructural.v"
 
 module BancoPruebas;
-   wire aclk, bclk, cclk, valid0, valid1, valid2, valid3, valid_out0, valid_out1, valid_out_l2;
-   wire [7:0] data_in0, data_in1, data_in2, data_in3, data_out0, data_out1, data_out_l2;
+   wire aclk, bclk, cclk, valid0, valid1, valid2, valid3, valid_out0, valid_out1,valid_out2,valid_out3, valid_out_l2;
+   wire [7:0] data_in0, data_in1, data_in2, data_in3, data_out0, data_out1, data_out2, data_out3, data_out_l2;
 
    probador probador (
 		      .aclk (aclk),
@@ -52,5 +53,20 @@ module BancoPruebas;
 		    .valid_out0 (valid_out_l2),
 		    .data_out0 (data_out_l2));
 
-   
+   muxL2_estr m_estr(
+		    .aclk (aclk), 
+		    .bclk (bclk), 
+		    .valid0 (valid0), 
+		    .valid1 (valid1), 
+		    .data_in0 (data_in0), 
+		    .data_in1 (data_in1), 
+	  	    .valid_out0 (valid_out0), 
+		    .valid_out1 (valid_out1), 
+		    .valid_out2 (valid_out2), 
+		    .valid_out3 (valid_out3), 
+		    .data_out0 (data_out0), 
+		    .data_out1 (data_out1), 
+		    .data_out2 (data_out2), 
+		    .data_out3 (data_out3));
+
 endmodule // BancoPruebas
